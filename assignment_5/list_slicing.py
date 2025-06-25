2202666711dbf100f69458dfb7c15ee0352138f2
SREEMOYEE CHATTERJEE <123591219+sreemoyee-chatterjee9@users.noreply.github.com>
2025-06-25 22:23:40 +0530

    Create list_slicing.py

---
+++ assignment_5/list_slicing.py
@@ -0,0 +1,6 @@
+original_list=list(range(1,11))
+print(f"Original List: {original_list}")
+extracted_list=original_list[0:5]
+print(f"Extracted first five elements:{extracted_list}")
+reversed_list=extracted_list[::-1]
+print(f"Reversed extracted elements:{reversed_list}")
