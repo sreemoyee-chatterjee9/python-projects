9d626806a6f0f8476b843eb360038ae51f1a8732
SREEMOYEE CHATTERJEE <123591219+sreemoyee-chatterjee9@users.noreply.github.com>
2025-06-25 12:21:04 +0530

    Create personalized_greetings.py

---
+++ assignment_1/personalized_greetings.py
@@ -0,0 +1,10 @@
+# Personalized Greetings
+
+#Step 1 : User's Input
+
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+
+#Step 2 : Concatenation and print
+
+print(f"Hello {first_name} " + f"{last_name}! Welcome to the Python Program.")
